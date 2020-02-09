92ae15c5e3c230b9e94524dae0b3cecba108eb86
aditya <adiyogisiddha@gmail.com>
2020-02-09 20:56:13 +0530

    mulitple rows insert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,7 @@
-insert into product (id,
-    name ,
-    product_type ,
-    brand ,
-    price ,
-    size ,
-    quantity ,
-    created_date ,
-    LAST_MODIFIED_DATE)
-values (1, 'SHIRT', 'CLOTH', 'WestSide', 1500, 's', 10, current_date, current_date);
+insert into product (ID, NAME, PRODUCT_TYPE, BRAND, PRICE, SIZE, QUANTITY, CREATED_DATE, LAST_MODIFIED_DATE)
+values
+ (1, 'SHIRT', 'CLOTH', 'WestSide', 500, 's', 10, current_date, current_date),
+ (2, 'SHIRT', 'CLOTH', 'WestSide', 1000, 'M', 10, current_date, current_date),
+ (3, 'SHIRT', 'CLOTH', 'WestSide', 1500, 'L', 10, current_date, current_date),
+ (4, 'SHIRT', 'CLOTH', 'WestSide', 2000, 'XL', 10, current_date, current_date),
+ (5, 'SHIRT', 'CLOTH', 'WestSide', 2500, 'XXL', 10, current_date, current_date);
