5ab6d76c8e29221fb462236e252a0faf3b5a512f
609734641 <aditya.ajmera@bt.com>
2020-02-10 14:29:52 +0530

    fixing test and changing default context

--- src/test/java/com/ecom/prodsearch/web/controller/ProductControllerTest.java
+++ src/test/java/com/ecom/prodsearch/web/controller/ProductControllerTest.java
@@ -1,20 +1,25 @@
-/*
 package com.ecom.prodsearch.web.controller;
 
 import com.ecom.prodsearch.service.IProducts;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductController.class)
+@ComponentScan(basePackages = {"com.ecom.prodsearch.web.mapper"})
 class ProductControllerTest {
 
     @Autowired
@@ -23,11 +28,12 @@ class ProductControllerTest {
     @Autowired
     ObjectMapper mapper;
 
-    @Autowired
+    @MockBean
     private IProducts products;
 
     @Test
     void getAll() throws Exception {
+        Mockito.when(products.getAll()).thenReturn(new ArrayList<>());
         mockMvc.perform(get("/api/v1/product/all").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
-}*/
+}
