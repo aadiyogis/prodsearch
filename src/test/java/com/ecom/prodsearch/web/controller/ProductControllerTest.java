d673d47b1770a699edf1ed3d8d7f8dc00c73c939
aditya <adiyogisiddha@gmail.com>
2020-02-10 19:36:59 +0530

    adding documentation

--- src/test/java/com/ecom/prodsearch/web/controller/ProductControllerTest.java
+++ src/test/java/com/ecom/prodsearch/web/controller/ProductControllerTest.java
@@ -3,19 +3,29 @@ package com.ecom.prodsearch.web.controller;
 import com.ecom.prodsearch.service.IProducts;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(ProductController.class)
 @ComponentScan(basePackages = {"com.ecom.prodsearch.web.mapper"})
 class ProductControllerTest {
@@ -32,6 +42,16 @@ class ProductControllerTest {
     @Test
     void getAll() throws Exception {
         when(products.getAll()).thenReturn(new ArrayList<>());
-        mockMvc.perform(get("/api/v1/product/all").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/product/all").
+                param("isCold","yes")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("v1/product/all",
+                    requestParameters(
+                            parameterWithName("isCold").description("Do you need cold beer?")
+                    ), responseFields(
+                            fieldWithPath("name")
+                        )
+                ));
     }
 }
