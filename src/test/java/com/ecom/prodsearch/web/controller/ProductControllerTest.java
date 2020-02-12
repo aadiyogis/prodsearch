05bff39d0e217407759f356d355bda90e1925759
aditya <adiyogisiddha@gmail.com>
2020-02-12 10:45:38 +0530

    adding validation of fields

--- src/test/java/com/ecom/prodsearch/web/controller/ProductControllerTest.java
+++ src/test/java/com/ecom/prodsearch/web/controller/ProductControllerTest.java
@@ -1,5 +1,6 @@
 package com.ecom.prodsearch.web.controller;
 
+import com.ecom.prodsearch.domain.Product;
 import com.ecom.prodsearch.service.IProducts;
 import com.ecom.prodsearch.web.model.ProductDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,9 +11,14 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -24,6 +30,8 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RestDocumentationExtension.class)
@@ -68,27 +76,45 @@ class ProductControllerTest {
     void saveProduct() throws Exception {
         ProductDto shirt = createProductDto();
         String shirtProd = mapper.writeValueAsString(shirt);
+        ResponseEntity<ProductDto> productDtoResponseEntity = new ResponseEntity<>(shirt, HttpStatus.CREATED);
+        ConstrainedFields fields = new ConstrainedFields(ProductDto.class);
+
         mockMvc.perform(post("/api/v1/product")
                 .accept(MediaType.APPLICATION_JSON)
                 .content(shirtProd)
                 .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isCreated())
+//                .andExpect(content().string(shirtProd))
                 .andDo(document("v1/product",
                         requestFields(
-                                fieldWithPath("name").description("name of product"),
-                                fieldWithPath("brand").description("brand of product"),
-                                fieldWithPath("productType").description("product type"),
-                                fieldWithPath("price").description("price of product"),
-                                fieldWithPath("size").description("size of product"),
-                                fieldWithPath("quantity").ignored()
-                        ),
-                        responseFields(
-                                fieldWithPath("name").description("name of product"),
-                                fieldWithPath("brand").description("brand of product"),
-                                fieldWithPath("productType").description("product type"),
-                                fieldWithPath("price").description("price of product"),
-                                fieldWithPath("size").description("size of product"),
-                                fieldWithPath("quantity").description("quantity of product created")
-                        )));
+                                fields.withPath("name").description("name of product"),
+                                fields.withPath("brand").description("brand of product"),
+                                fields.withPath("productType").description("product type"),
+                                fields.withPath("price").description("price of product"),
+                                fields.withPath("size").description("size of product"),
+                                fields.withPath("quantity").ignored()
+//                        ),
+//                        responseFields(
+//                                fields.withPath("name").description("name of product"),
+//                                fields.withPath("brand").description("brand of product"),
+//                                fields.withPath("productType").description("product type"),
+//                                fields.withPath("price").description("price of product"),
+//                                fields.withPath("size").description("size of product"),
+//                                fields.withPath("quantity").description("quantity of product created")
+                        )
+                ));
+    }
+
+    private static class ConstrainedFields {
+        private final ConstraintDescriptions constraintDescriptions;
+
+        public ConstrainedFields(Class<?> input) {
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+
+        private FieldDescriptor withPath(String path) {
+            return fieldWithPath(path).attributes(key("constraints")
+                    .value(StringUtils.collectionToDelimitedString(this.constraintDescriptions.descriptionsForProperty(path), ". ")));
+        }
     }
 }
